e223933068d22e8fb5b2c9cc4aadd3aec09b8684
kena <kena@raysengine.com>
2026-03-25 11:27:28 +0800

    02:validation layers

---
+++ project/02_validation_layers.cpp
@@ -0,0 +1,116 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+
+#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
+#	include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
+
+#define GLFW_INCLUDE_VULKAN        // REQUIRED only for GLFW CreateWindowSurface.
+#include <GLFW/glfw3.h>
+
+const uint32_t WIDTH  = 800;
+const uint32_t HEIGHT = 600;
+
+const std::vector<char const*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUF
+	constexpr bool enableValidationLayers = false;
+#else
+	constexpr bool enableValidationLayers = true;
+#endif
+
+class HelloTriangleApplication
+{
+  public:
+	void run()
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+  private:
+	GLFWwindow *window = nullptr;
+	vk::raii::Context context;
+    vk::raii::Instance instance = nullptr;
+
+	void initWindow()
+	{
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+    void createInstance()
+    {
+        constexpr vk::ApplicationInfo appInfo{
+            .pApplicationName = "Hello Triangle",
+            .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+            .pEngineName = "No Engine",
+            .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+            .apiVersion = vk::ApiVersion14
+        };
+
+		uint32_t glfwExtensionCount = 0;
+		auto     glfwExtensions     = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		vk::InstanceCreateInfo createInfo{
+			.pApplicationInfo = &appInfo,
+			.enabledLayerCount = static_cast<uint32_t>(validationLayers.size()),
+			.ppEnabledLayerNames = validationLayers.data(),
+			.enabledExtensionCount = glfwExtensionCount,
+			.ppEnabledExtensionNames = glfwExtensions
+		};
+
+		instance = vk::raii::Instance(context, createInfo);
+    }
+
+	void initVulkan()
+	{
+        createInstance();
+	}
+
+	void mainLoop()
+	{
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+	}
+
+	void cleanup()
+	{
+		glfwDestroyWindow(window);
+
+		glfwTerminate();
+	}
+};
+
+int main()
+{
+	std::cout<<"hello world"<<std::endl;
+	try
+	{
+		HelloTriangleApplication app;
+		app.run();
+
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
